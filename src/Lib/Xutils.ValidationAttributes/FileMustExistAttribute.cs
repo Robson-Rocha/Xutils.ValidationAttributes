223fb9d7aaa85cc54f68e70867d65527e6141035
Robson Rocha de Araújo <rrocha.araujo@gmail.com>
2019-11-21 22:24:33 -0300

    Initial Commit

---
+++ src/Lib/Xutils.ValidationAttributes/FileMustExistAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace DeepRen
+{
+    /// <summary>
+    /// Validates if the file represented by the annotated property or field exists in disk
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class FileMustExistAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string path = value as string;
+            return !string.IsNullOrWhiteSpace(path) && File.Exists(path);
+        }
+    }
+}
---
+++ src/Lib/Xutils.ValidationAttributes/PathMustExistAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace DeepRen
+{
+    /// <summary>
+    /// Validates if the path represented by the annotated property or field exists in disk
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class PathMustExistAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string path = value as string;
+            return !string.IsNullOrWhiteSpace(path) && Directory.Exists(path);
+        }
+    }
+}
